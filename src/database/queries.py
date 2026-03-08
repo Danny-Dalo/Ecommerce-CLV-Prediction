043f293257aba42288df42fd75700a177dd10b93
Danny-Dalo <ighodalodanny7@gmail.com>
2026-03-08 10:45:17 +0100

    editted how database sql files are called, can run and call the frozen code now

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,10 +1,21 @@
 from src.database.db_engine import engine
 import pandas as pd
+from sqlalchemy import text
 
-query = """
-    SELECT * FROM clv_data;
-"""
 
-clv_data = pd.read_sql(query, engine)
 
-clv_data.to_parquet("data/processed/clv_table_data.parquet")
+with open("sql/TABLE_VIEWS_02.sql", "r") as f:
+    view_sql = f.read()
+
+with engine.begin() as conn:
+    conn.execute(text(view_sql))
+
+
+
+clv_data = pd.read_sql("SELECT * FROM clv_data;", engine)
+clv_data.to_parquet("data/2021_customer_data.parquet")
+
+
+clv_target = pd.read_sql("SELECT * FROM clv_target", engine)
+clv_target.to_parquet("data/2022_customer_data.parquet")
+
